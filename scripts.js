2652da4f7b2d2f16b86608b1d35128dc41b8b074
Kincsoo <66359013+Kincsoo@users.noreply.github.com>
2020-06-09 15:55:33 +0200

    Javascript exercise

---
+++ scripts.js
@@ -0,0 +1,29 @@
+console.log('This is the JavaScript');
+let names = ['Klára', 'Sára', 'Kata']
+let object = {
+    title: "My first object",
+    description: "I just give a try.",
+    colors: ['green', 'blue', 'yellow'],
+    hasManyColors: "I don't understand yet.",
+    }
+
+console.log(names)
+console.log(object)
+let age = 25
+
+if (age >= 18) {
+    console.log('I am an adult.')
+} else {
+    console.log('I am a child.')
+}
+
+object.hasManyColors = object.colors.length > 3
+
+console.log(object)
+
+console.log (object.colors)
+let result = 10/0
+console.log(result)
+
+console.log(5%3)
+
